d0ad724bfd1bbc061c44f6954f001b82ba30e849
kabumoka-alt <kabumoka@gmail.com>
2026-06-17 03:53:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import os
 import requests
+import time
 
 token = os.getenv("TELEGRAM_TOKEN")
 chat_id = os.getenv("CHAT_ID")
 
-url = f"https://api.telegram.org/bot{token}/sendMessage"
+while True:
+    try:
+        requests.get(
+            f"https://api.telegram.org/bot{token}/sendMessage",
+            params={
+                "chat_id": chat_id,
+                "text": "🚀 Railway 살아있음 테스트"
+            }
+        )
+        print("sent")
+    except Exception as e:
+        print(e)
 
-requests.get(
-    url,
-    params={
-        "chat_id": chat_id,
-        "text": "🚀 Railway 테스트 성공!"
-    }
-)
-
-print("완료")
+    time.sleep(60)
